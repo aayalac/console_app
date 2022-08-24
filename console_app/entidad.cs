2c8c7ffa39c913b9185acd34aefdd38a5b4c2850
migue <migue@DESKTOP-FHMAVA1>
2022-08-24 13:59:55 -0500

    Agregar archivos de proyecto.

---
+++ console_app/entidad.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace console_app
+{
+    internal class entidad
+    {
+        public string Name { get; protected set; }
+
+        protected entidad(string Name)
+        {
+            this.Name = Name;
+        }
+    }
+}
